5165a376cedea504ff839ef7be2abf6f52154989
-e <77801870+byemc@users.noreply.github.com>
2022-05-29 11:19:08 +0100

    YES! WORKING JAVASCRIPT

--- src/js/check_online.js
+++ src/js/check_online.js
@@ -4,5 +4,18 @@ fetch('https://bbapi.byemc.xyz/info')
         return response.json();
     })
     .then(online_status => {
-        console.log(online_status);
+        console.log(online_status.online);
+        if (online_status.online == true) {
+            document.getElementById("onl-indicator").innerHTML = "online";
+            document.getElementById("onl-indicator").style.color = "green";
+        } else {
+            document.getElementById("onl-indicator").innerHTML = "offline";
+            document.getElementById("onl-indicator").style.color = "red";
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        document.getElementById("onl-indicator").innerHTML = "offline";
+        document.getElementById("onl-indicator").style.color = "red";
     });
+

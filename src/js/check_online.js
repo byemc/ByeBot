85fad47795c9048af815e1b382aeacee549fbd9d
-e <77801870+byemc@users.noreply.github.com>
2022-05-29 11:07:36 +0100

    :)

--- src/js/check_online.js
+++ src/js/check_online.js
@@ -1,20 +1,8 @@
 
-function checkOnline() {
-    return fetch('https://bbapi.byemc.xyz/info').then(response => {
-        if (response.contentType === 'application/json') {
-            return response.json();
-        } else {
-            return {};
-        }
+fetch('https://bbapi.byemc.xyz/info')
+    .then(response => {
+        return response.json();
+    })
+    .then(online_status => {
+        console.log(online_status);
     });
-}
-
-bbonline = JSON.parse(checkOnline());
-if (bbonline.online) {
-    document.getElementById('onl-indicator').innerHTML = 'Online';
-    document.getElementById('onl-indicator').style.color = '#00ff00';
-}
-else {
-    document.getElementById('onl-indicator').innerHTML = 'Offline';
-    document.getElementById('onl-indicator').style.color = '#ff0000';
-}
